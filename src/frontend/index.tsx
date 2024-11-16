ca98cea8dbb37cd79722f3d384bb30aa076cba96
Hichem Fantar <hichemfantar2049@gmail.com>
2024-11-16 18:44:49 +0100

    feat: add error handling for mutations in QueryClient configuration

--- src/frontend/index.tsx
+++ src/frontend/index.tsx
@@ -5,15 +5,32 @@ import { Toaster } from "@/components/ui/toaster";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { toast } from "@/hooks/use-toast";
 
 const container = document.getElementById("root");
 
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {},
+    mutations: {
+      onError: (error) =>
+        toast({
+          title: error.name,
+          description: error.message,
+          variant: "destructive",
+        }),
+    },
+  },
+});
 
 if (container) {
   const root = createRoot(container);
 
-  root.render(
+  root.render(<Init />);
+}
+
+function Init() {
+  return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <TooltipProvider delayDuration={0}>
